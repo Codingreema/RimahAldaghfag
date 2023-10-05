d5a9b56dd9a250fdecca71843abcfe2bbcea484b
Codingreema <dimaabd383@gmail.com>
2023-10-05 11:59:22 +0300

    Initial Commit

---
+++ RimahAldaghfag/RimahAldaghfagApp.swift
@@ -0,0 +1,17 @@
+//
+//  RimahAldaghfagApp.swift
+//  RimahAldaghfag
+//
+//  Created by Rimah on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct RimahAldaghfagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
